6fe7965005b279572895f780acf016490c873277
Guillaume Zucchi <gzucchi.pro@gmail.com>
2017-04-14 11:12:50 +0200

    Update test.js
    Modification de la fonction deplace() : le calcul de l'index de la case suivante est OK.

--- js/test.js
+++ js/test.js
@@ -1,3 +1,4 @@
+//================================================================================================================================================//
 //création des variables
 
 var tabPosition = [];
@@ -8,13 +9,13 @@ var nombreCaseX = 10;
 var nombreCaseY = 10;
 
 
-
 //================================================================================================================================================//
 
 // fonction qui retourne un entier aléatoire entre 1 et max
 function generate_random(max) {
     return Math.floor((Math.random() * max) + 1);
 }
+
 
 //================================================================================================================================================//
 
@@ -30,7 +31,7 @@ function generate_map() {
                 $("#map").append("<img src='../img/casevide.png' class='casevideClass' id='casevide'>");
                 perso1_sur_la_map = true;
                 tabPosition.push(3); // id du perso1
-            } else if (random == 12 && perso2_sur_la_map == false) { // si random est égal à 12 et que le perso2 n'est pas encore sur la map
+            /*} else if (random == 12 && perso2_sur_la_map == false) { // si random est égal à 12 et que le perso2 n'est pas encore sur la map
                 $("#map").append("<img src='../img/perso2.png' class='persoClass' id='perso2' style='left:" + 100 * j + "px; top:" + 100 * i + "px'>");
                 $("#map").append("<img src='../img/casevide.png' class='casevideClass' id='casevide'>");
                 perso2_sur_la_map = true;
@@ -54,7 +55,7 @@ function generate_map() {
             } else if ((random == 7 || random == 13 || random == 9 || random == 15)) { // si random est égal à 7 ou si random est égal à 13 on insert un bloc
                 $("#map").append("<img src='../img/bloc.png' class='blocClass' id='bloc'>");
                 tabPosition.push(1); // id du bloc
-            } else { // pour tout autre valeur de random on insert systématiquement une case vide
+            */} else { // pour tout autre valeur de random on insert systématiquement une case vide
                 $("#map").append("<img src='../img/casevide.png' class='casevideClass' id='casevide'>");
                 tabPosition.push(0); // id de la case vide
             }
@@ -72,23 +73,23 @@ generate_map(nombreCaseX * nombreCaseY);
 //fonction qui permet de gérer les déplacements du personnage 1
 function deplace() {
 
-
     var ligne = parseInt($('#perso1').css('top')) / 100; // position en x
     var colonne = parseInt($('#perso1').css('left')) / 100; // position en Y
     var longueur = 10; //10 cases par lignes
-    var index = ligne * longueur + colonne; // position actuelle du perso avant déplacement
+    var index = ligne * colonne + longueur; // position actuelle du perso avant déplacement
 
     $(document).keydown(function(e) { //ou est-ce que se trouve le perso
+        console.log("notre case à l'index " + index + " avant déplacement");
 
         if (e.which == 37) { // Vers la gauche
-            console.log("gauche");
+            console.log("déplacement à gauche");
             colonne--; // on se dirige vers la colonne précedante
-            index = ligne * colonne + longueur; // l'index de la case suivant
-            console.log("l' index de la case est" + " " + index);
+            index = ligne * longueur + colonne;  // l'index de la case suivant
             if (tabPosition[index] == 1) { // bloc
                 console.log("Vous faîtes face à un bloc");
             } else if ((tabPosition[index] == 0) || (tabPosition[index] == 2) || (tabPosition[index] == 4)) {
                 $('#perso1').css('left', parseInt($('#perso1').css('left')) - 100); // donc on se déplace
+                console.log("après déplacement nous sommes sur la case qui a l' index " + " " + index);
                 if (tabPosition[index] == 2) { // arme
                     console.log("Vous vous équipez d'une arme"); // ici fonction equipe()
                 }
@@ -119,7 +120,7 @@ function deplace() {
         if (e.which == 39) { // Vers la droite
             console.log("droite");
             colonne++; // on se dirige vers la colonne suivant
-            index = ligne * colonne + longueur; // l'index de la case suivant
+            index = ligne * longueur + colonne;  // l'index de la case suivant
             console.log("l' index de la case est" + " " + index);
             if (tabPosition[index] == 1) {
                 console.log("Vous faîtes face à un bloc");
@@ -155,3 +156,4 @@ function deplace() {
 }
 
 deplace();
+
