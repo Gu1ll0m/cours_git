1769ec102fae87c5c141a919a0448eba7578f8de
Guillaume Zucchi <gzucchi.pro@gmail.com>
2017-04-19 12:18:04 +0200

    Update test.js
    MAJ du fichier script.js

--- js/test.js
+++ js/test.js
@@ -8,7 +8,6 @@ var nb_arme_sur_la_map = 0;
 var nombreCaseX = 10;
 var nombreCaseY = 10;
 
-
 //================================================================================================================================================//
 
 // fonction qui retourne un entier aléatoire entre 1 et max
@@ -16,11 +15,11 @@ function generate_random(max) {
     return Math.floor((Math.random() * max) + 1);
 }
 
-
 //================================================================================================================================================//
 
 // fonction qui permet la création de la map
 function generate_map() {
+
     for (var i = 0; i < nombreCaseX; i++) {
         for (var j = 0; j < nombreCaseY; j++) {
             var map = Object.create(Map); // création de la map
@@ -64,91 +63,111 @@ function generate_map() {
     }
 }
 
-
 generate_map(nombreCaseX * nombreCaseY);
-
 
 //================================================================================================================================================//
 
-//fonction qui permet de gérer les déplacements du personnage 1
+//fonction qui permet de gérer les déplacements du personnage
 function deplace() {
 
-    var ligne = parseInt($('#perso1').css('top')) / 100; // position en x
-    var colonne = parseInt($('#perso1').css('left')) / 100; // position en Y
-    var longueur = 10; //10 cases par lignes
-    var index = ligne * colonne + longueur; // position actuelle du perso avant déplacement
+    $(document).keydown(function(e) { // la fonction se déclenche dès que l'utilisateur prèse une touche
 
-    $(document).keydown(function(e) { //ou est-ce que se trouve le perso
-        console.log("notre case à l'index " + index + " avant déplacement");
+        var ligne = parseInt($('#perso1').css('top')) / 100; // position en x
+        var colonne = parseInt($('#perso1').css('left')) / 100; // position en Y
+        var longueur = 10; //10 cases par lignes
+        var index = ligne * colonne + longueur; // position actuelle du perso avant déplacement
+
+
+        function bloc() {
+            $('#état').html('Vous faîtes face à un bloc').fadeIn('slow').delay(2000).fadeOut('slow'); // ajoute le message dans un span et le fait disparaitre au bout de 2 secondes
+        }
+
+        function combat() { /* ici fonction combat()*/
+            $('#état').html('FIGHT').fadeIn('slow').delay(2000).fadeOut('slow');
+        }
+
+        function arme() { /* ici la fonction equip()*/
+            $('#état').html('Vous vous équipez d une arme').fadeIn('slow').delay(2000).fadeOut('slow');
+        }
+
+        function bord() {
+            $('#état').html('Vous faîtes face à un bord').fadeIn('slow').delay(2000).fadeOut('slow');
+        }
 
         if (e.which == 37) { // Vers la gauche
-            console.log("déplacement à gauche");
+            $('#rapport').html('Bouton pressé : flêche gauche');
             colonne--; // on se dirige vers la colonne précedante
-            index = ligne * longueur + colonne;  // l'index de la case suivant
+            index = ligne * longueur + colonne; // l'index de la case suivant
             if (tabPosition[index] == 1) { // bloc
-                console.log("Vous faîtes face à un bloc");
+                bloc();
             } else if ((tabPosition[index] == 0) || (tabPosition[index] == 2) || (tabPosition[index] == 4)) {
                 $('#perso1').css('left', parseInt($('#perso1').css('left')) - 100); // donc on se déplace
-                console.log("après déplacement nous sommes sur la case qui a l' index " + " " + index);
+                console.log("après déplacement index " + " " + index);;
                 if (tabPosition[index] == 2) { // arme
-                    console.log("Vous vous équipez d'une arme"); // ici fonction equipe()
+                    arme();
                 }
                 if ((tabPosition[index] == 3) || (tabPosition[index] == 4)) { // personnage
-                    console.log("FIGHT"); // ici fonction combat()
+                    combat();
+                }
+                if ((tabPosition == 0) || (tabPosition == 10) || (tabPosition == 20) || (tabPosition == 30) || (tabPosition == 40) || (tabPosition == 50) || (tabPosition == 60) || (tabPosition == 70) || (tabPosition == 80) || (tabPosition == 90)) {
+                    bord();
                 }
             }
         }
 
         if (e.which == 38) { // Vers le haut
-            console.log("haut");
+            $('#rapport').html('Bouton pressé : flêche haut');
             ligne--; // on se dirige vers la ligne précédante
             index = ligne * longueur + colonne; // l'index de la case suivant
-            console.log("l' index de la case est" + " " + index);
             if (tabPosition[index] == 1) {
-                console.log("Vous faîtes face à un bloc");
+                bloc();
             } else if ((tabPosition[index] == 0) || (tabPosition[index] == 2) || (tabPosition[index] == 4)) {
                 $('#perso1').css('top', parseInt($('#perso1').css('top')) - 100); // donc on se déplace
+                console.log("après déplacement index " + " " + index);
                 if (tabPosition[index] == 2) {
-                    console.log("Vous vous équipez d'une arme"); // ici fonction equipe()
+                    arme();
                 }
                 if ((tabPosition[index] == 3) || (tabPosition[index] == 4)) { // personnage
-                    console.log("FIGHT"); // ici fonction combat()
+                    combat(); // ici fonction combat()
                 }
             }
         }
 
         if (e.which == 39) { // Vers la droite
-            console.log("droite");
+            $('#rapport').html('Bouton pressé : flêche droite');
             colonne++; // on se dirige vers la colonne suivant
-            index = ligne * longueur + colonne;  // l'index de la case suivant
-            console.log("l' index de la case est" + " " + index);
+            index = ligne * longueur + colonne; // l'index de la case suivant
             if (tabPosition[index] == 1) {
-                console.log("Vous faîtes face à un bloc");
+                bloc();
             } else if ((tabPosition[index] == 0) || (tabPosition[index] == 2) || (tabPosition[index] == 4)) {
                 $('#perso1').css('left', parseInt($('#perso1').css('left')) + 100); // donc on se déplace
+                console.log("après déplacement index " + " " + index);
                 if (tabPosition[index] == 2) {
-                    console.log("Vous vous équipez d'une arme"); // ici fonction equipe()
+                    arme(); // ici fonction equipe()
                 }
                 if ((tabPosition[index] == 3) || (tabPosition[index] == 4)) { // personnage
-                    console.log("FIGHT"); // ici fonction combat()
+                    combat(); // ici fonction combat()
+                }
+                if ((tabPosition == 9) || (tabPosition == 19) || (tabPosition == 29) || (tabPosition == 39) || (tabPosition == 49) || (tabPosition == 59) || (tabPosition == 69) || (tabPosition == 79) || (tabPosition == 89) || (tabPosition == 99)) {
+                    bord();
                 }
             }
         }
 
         if (e.which == 40) { // Vers le bas
-            console.log("bas");
+            $('#rapport').html('Bouton pressé : flêche bas');
             ligne++; // on se dirige vers la ligne suivante
             index = ligne * longueur + colonne; // l'index de la case suivant
-            console.log("l' index de la case est" + " " + index);
             if (tabPosition[index] == 1) {
-                console.log("Vous faîtes face à un bloc");
+                bloc();
             } else if ((tabPosition[index] == 0) || (tabPosition[index] == 2) || (tabPosition[index] == 4)) {
                 $('#perso1').css('top', parseInt($('#perso1').css('top')) + 100); // donc on se déplace
+                console.log("après déplacement index " + " " + index);
                 if (tabPosition[index] == 2) {
-                    console.log("Vous vous équipez d'une arme"); // ici fonction equipe()
+                    arme(); // ici fonction equipe()
                 }
                 if ((tabPosition[index] == 3) || (tabPosition[index] == 4)) { // personnage
-                    console.log("FIGHT"); // ici fonction combat()
+                    combat(); // ici fonction combat()
                 }
             }
         }
