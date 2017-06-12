3765afd6c60f068afdaeedeefab857f84a8dfc54
Gu1ll0m <gzucchi.pro@gmail.com>
2017-06-12 13:02:05 +0200

    Create perso_v2.js

--- v2/js/perso_v2.js
+++ v2/js/perso_v2.js
@@ -27,49 +27,12 @@ function perso() {
 
 
 
-var personnage = {
-  // Initialise le personnage
-  init: function(nom, sante) {
-    this.nom = nom;
-    this.sante = sante;
-    if (degat) {
-      this.degat = degat;
-    } // si le personnage est équipé d'une arme
-  },
-
-  // Renvoie la description du personnage
-  decrire: function() {
-    if (this.degat) {
-      return (this.nom + " a " + this.sante + " points de vie et inflige " + this.degat + " points de dégât avec son arme");
-    } // si 4 variables
-    else return (this.nom + " a " + this.sante + " points de vie"); // si 3 variables
-  },
+function Personnage (nom, sante, degat) {
+  this.nom = nom;
+  this.sante = sante;
+  this.degat = degat;
 }
 
-
-var perso1 = Object.create(personnage);
-perso1.init("Alien", 100);
-
-var perso2 = Object.create(personnage);
-perso2.init("Predator", 100);
-
-console.log(perso1.decrire());
-console.log(perso2.decrire());
-
-
-// attaque un personnage cible
-function attaquer(cible) {
-  if (this.sante > 0) {
-    var degats = this.degat;
-    console.log(this.nom + " attaque " + cible.nom + " et lui fait " + degats + " points de dégâts");
-    cible.sante = cible.sante - degats;
-    if (cible.sante > 0) {
-      console.log(cible.nom + " a encore " + cible.sante + " points de vie");
-    } else {
-      cible.sante = 0;
-      console.log(cible.nom + " est mort !");
-    }
-  } else {
-    console.log(this.nom + " ne peut pas attaquer : il est mort...");
-  }
-};
+// On crée des variables qui vont contenir une instance de l'objet Person :
+var perso1 = new Personnage('Alien', 100, 10, []);
+var perso2 = new Personnage('Predator', 100, 10, []);
