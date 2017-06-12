4f5a00a2e32828486025a37238fa008105770620
Gu1ll0m <gzucchi.pro@gmail.com>
2017-06-12 12:59:43 +0200

    Create arme_v2.js

--- v2/js/arme_v2.js
+++ v2/js/arme_v2.js
@@ -1,30 +1,23 @@
-var arme = {
-    // Initialise les armes
-    init: function(nom, degat) {
-        this.nom = nom;
-        this.degat = degat;
-    },
+//================================================================================================================================================//
+//============================================================= Objet Arme ======================================================================//
+//================================================================================================================================================//
 
-    // Renvoie la description de l'arme
-    decrire: function() {
-        var description = this.nom + " fait " + this.degat + " points de dégâts";
-        return description;
-    }
-};
+function Arme (nom, degat, url) {
+  this.nom = nom;
+  this.degat = degat;
+  this.url = url;
+}
 
-var arme1 = Object.create(arme);
-arme1.init("lanceur de bisoux intergalactique", 10);
+// Renvoie la description de l'arme
+function descriptionArme() {
+    var description = this.nom + " fait " + this.degat + " points de dégâts";
+    return description;
+}
 
-var arme2 = Object.create(arme);
-arme2.init("lanceur de cailloux intergalactique", 25);
+var arme1 = new Arme ("lanceur de bisoux intergalactique", 10, src="../img/arme1.png");
 
-var arme3 = Object.create(arme);
-arme3.init("patator intergalactique", 50);
+var arme2 = new Arme ("lanceur de cailloux intergalactique", 25, src="../img/arme2.png");
 
-var arme4 = Object.create(arme);
-arme4.init("headshotter intergalactique", 100);
+var arme3 = new Arme ("patator intergalactique", 50, src="../img/arme3.png");
 
-console.log(arme1.decrire());
-console.log(arme2.decrire());
-console.log(arme3.decrire());
-console.log(arme4.decrire());
+var arme4 = new Arme ("headshotter intergalactique", 100, src="../img/arme4.png");
