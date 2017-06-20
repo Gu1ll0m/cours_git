d52d5a234199d36420e009f3afdcbcb02ad67189
Gu1ll0m <gzucchi.pro@gmail.com>
2017-06-20 10:17:16 +0200

    Update arme_v2.js
    Modification de la structure de l'objet Arme afin d' avoir la même méthode que pour l' objet Perso.

--- v2/js/arme_v2.js
+++ v2/js/arme_v2.js
@@ -2,33 +2,18 @@
 //============================================================= Objet Arme ======================================================================//
 //================================================================================================================================================//
 
-var arme = {
+function Arme() {
+
   // Initialise les armes
-  init: function(nom, degat ) {
+  this.initArme = function(nom, degat, elm ) {
     this.nom = nom;
     this.degat = degat;
+    this.elm = elm;
   },
 
   // Renvoie la description de l'arme
-  decrire: function() {
+  this.decrireArme = function() {
     var description = this.nom + " fait " + this.degat + " points de dégâts";
     return description;
   }
 };
-
-var arme1 = Object.create(arme);
-arme1.init("lanceur de bisoux intergalactique", 10);
-
-var arme2 = Object.create(arme);
-arme2.init("lanceur de cailloux intergalactique", 25);
-
-var arme3 = Object.create(arme);
-arme3.init("patator intergalactique", 50);
-
-var arme4 = Object.create(arme);
-arme4.init("headshotter intergalactique", 100);
-
-console.log(arme1.decrire());
-console.log(arme2.decrire());
-console.log(arme3.decrire());
-console.log(arme4.decrire());
