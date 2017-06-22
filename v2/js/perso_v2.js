e6ec162db02671c42e7b560e204f0577f6dd62df
Gu1ll0m <gzucchi.pro@gmail.com>
2017-06-22 12:40:56 +0200

    Update perso_v2.js
    Rajout l.34

--- v2/js/perso_v2.js
+++ v2/js/perso_v2.js
@@ -21,20 +21,19 @@ function Perso() {
   //this.armeActuelle;
 
   // lance la fct mvt() si index n'est ni un bloc ni un perso
-  this.deplace = function(old_index, index, mvt) {
+  this.deplace = function(old_index, index, mvt, map) {
     var self = this;
     //console.log(self); // retourne la valeur de self
     if (tab_position[index] == 1) { // 1 est l' id des blocs
       alert("Vous faîtes face à un bloc");
     } 
-    else if ((tab_position[index] == 0) || (tab_position[index] == 2) || (tab_position[index] == 3) || (tab_position[index] == 4)) { 
+    else if ((tab_position[index] == 0) || (tab_position[index] == "arme1Class") || (tab_position[index] == "arme2Class") || (tab_position[index] == "arme3Class") || (tab_position[index] == "arme4Class") || (tab_position[index] == 3) || (tab_position[index] == 4)) { 
     // si on est face à une case vide, une arme, Alien ou Predator
 
-      if (tab_position[index] == 2) { // 2 est l' id des armes
-        alert("Vous vous équipez d'une arme"); 
+      if ((tab_position[index] == "arme1Class") || (tab_position[index] == "arme2Class") || (tab_position[index] == "arme3Class") || (tab_position[index] == "arme4Class")) { // id des armes
+        this.degat=map.arsenal[tab_position[index]].degat;
+        alert("Vous vous équipez d'une arme avec "+this.degat+" de dégâts supplémentaires");
         $carte.append("<div class='casevideClass' style='left:" + (index % 10) * 100 + "px; top:" + Math.floor(index / 10) * 100 + "px ;position: absolute;'></div>"); // on remplace la case de l'arme par une case vide
-        Arme(); 
-        //equipArme();//TODO : la valeur des dégâts de l' arme doit remplacer la valeur des dégâts actuels du personnage
         
         if (mvt == "gauche") { // déplacement à gauche
           self.elm.css('left', parseInt(self.elm.css('left')) - 100);
@@ -55,7 +54,7 @@ function Perso() {
       }
 
 
-      else if ((tab_position[index] == 3) || (tab_position[index] == 4)) { // 3 et 4 sont les id des personnages
+      else if ((tab_position[index] == 3) || (tab_position[index] == 4)) { // "Alien" et 4 sont les id des personnages
         alert("FIGHT"); 
         fight(); // ici fonction fight() fight.js
       }
@@ -81,12 +80,11 @@ function Perso() {
       }
 
 
+      if (self.nom == "Alien") tab_position[index] = 3; // maj index de la case suivante (contient maintenant le perso)      
       if (self.nom == "Predator") tab_position[index] = 4; // maj index de la case suivante (contient maintenant le perso)
-      if (self.nom == "Alien") tab_position[index] = 3; // maj index de la case suivante (contient maintenant le perso)
       tab_position[old_index] = 0; // maj index de la case precedente (contient maintenant une case vide)
       game.verifieCompteur();
     }
   }
 
 }
-
