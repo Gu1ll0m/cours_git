0ec89f0099c8d518f8ca02953662d39e4515b350
Gu1ll0m <gzucchi.pro@gmail.com>
2017-06-26 19:02:00 +0200

    Update map_v2.js

--- v2/js/map_v2.js
+++ v2/js/map_v2.js
@@ -29,17 +29,17 @@ function map(nombre_case_X, nombre_case_Y) {
           tab_position.push(3); // 3 est l' id d' Alien
 
           this.perso1 = new Perso(); // function Perso() perso_v2.js
-          this.perso1.initPerso("Alien", 100, 10, $("#perso1")); // initialise Alien à partir du constructor Perso
+          this.perso1.initPerso("Alien", 100, 10, $("#perso1"),0); // initialise Alien à partir du constructor Perso
         }
 
         else if (random == 12 && this.perso2_sur_la_map == false) { // si random est égal à 12 et que le perso2 n'est pas encore sur la map
           $carte.append("<img src='../img/perso2_predator.png' class='persoClass' id='perso2' style='left:" + 100 * j + "px; top:" + 100 * i + "px'>"); // on ajoute la div Predator
           $carte.append("<div class='casevideClass'></div>"); // on ajoute une div casevide sous la div Predator
-          this.perso2_sur_la_map = true; 
+          this.perso2_sur_la_map = true;
           tab_position.push(4); // 4 est l'id de Predator
 
           this.perso2 = new Perso();
-          this.perso2.initPerso("Predator", 100, 10, $("#perso2")); // initialise Predator
+          this.perso2.initPerso("Predator", 100, 10, $("#perso2"),0); // initialise Predator
         }
 
         else if (random == 3 && this.nb_arme_sur_la_map < 1) { // si random est égal à 3 et que l' arme1 n'est pas encore sur la map
@@ -48,37 +48,37 @@ function map(nombre_case_X, nombre_case_Y) {
           tab_position.push("arme1Class"); // 2 est l' id des armes
 
           this.arme1 = new Arme(); // function Arme() arme_v2.js
-          this.arme1.initArme("lanceur de bisoux intergalactique", 25, $("#arme1")); // initialise l' arme 1 à partir du constructor Arme
+          this.arme1.initArme("lanceur de bisoux intergalactique", 15, $("#arme1")); // initialise l' arme 1 à partir du constructor Arme
           this.arsenal["arme1Class"] = this.arme1;
         }
 
         else if (random == 3 && this.nb_arme_sur_la_map < 2) { // si random est égal à 3 et que l' arme2 n'est pas encore sur la map
           $carte.append("<div class='arme2Class'></div>"); // on ajoute la div arme 2
-          this.nb_arme_sur_la_map++; 
-          tab_position.push("arme2Class"); 
+          this.nb_arme_sur_la_map++;
+          tab_position.push("arme2Class");
 
           this.arme2 = new Arme();
-          this.arme2.initArme("lanceur de cailloux intergalactique", 35, $("#arme2")); // initialise l' arme 2
+          this.arme2.initArme("lanceur de cailloux intergalactique", 20, $("#arme2")); // initialise l' arme 2
           this.arsenal["arme2Class"] = this.arme2;
         }
 
         else if (random == 3 && this.nb_arme_sur_la_map < 3) { // si random est égal à 3 et que l' arme3 n'est pas encore sur la map
           $carte.append("<div class='arme3Class'></div>"); // on ajoute la div arme 3
-          this.nb_arme_sur_la_map++; 
-          tab_position.push("arme3Class"); 
+          this.nb_arme_sur_la_map++;
+          tab_position.push("arme3Class");
 
           this.arme3 = new Arme();
-          this.arme3.initArme("patator intergalactique", 50, $("#arme3")); // initialise l' arme 3
+          this.arme3.initArme("patator intergalactique", 25, $("#arme3")); // initialise l' arme 3
           this.arsenal["arme3Class"] = this.arme3;
         }
 
         else if (random == 3 && this.nb_arme_sur_la_map < 4) { // si random est égal à 3 et que l' arme4 n'est pas encore sur la map
           $carte.append("<div class='arme4Class'></div>"); // on ajoute la div arme 4
           this.nb_arme_sur_la_map++;
-          tab_position.push("arme4Class"); 
+          tab_position.push("arme4Class");
 
           this.arme4 = new Arme();
-          this.arme4.initArme("headshotter intergalactique", 100, $("#arme4")); // initialise l' arme 4
+          this.arme4.initArme("headshotter intergalactique", 30, $("#arme4")); // initialise l' arme 4
           this.arsenal["arme4Class"] = this.arme4;
         }
 
@@ -91,7 +91,7 @@ function map(nombre_case_X, nombre_case_Y) {
           $carte.append("<div class='casevideClass'></div>"); // on ajoute une case vide
           tab_position.push(0); // 0 est l' id de la case vide
         }
-        //console.log("case" + " " + i + j); // indique l' index de la case
+        console.log("case" + " " + i + j); // indique l' index de la case
       }
     }
   };
