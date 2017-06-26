e9514e00f9d84a1928db531579e412ee895f31e4
Gu1ll0m <gzucchi.pro@gmail.com>
2017-06-26 19:03:28 +0200

    Update perso_v2.js

--- v2/js/perso_v2.js
+++ v2/js/perso_v2.js
@@ -5,7 +5,7 @@
 function Perso() {
 
   // initialise les personnages avec les paramètres nom santé dégât et elm qui correspond à l' élément html.
-  this.initPerso = function(nom, sante, degat, elm) {
+  this.initPerso = function(nom, sante, degat, elm, posture) {
     this.nom = nom;
     this.sante = sante;
     this.degat = degat;
@@ -18,8 +18,6 @@ function Perso() {
     var description = this.nom + "possède" + this.sante + "et fait " + this.degat + " points de dégâts avec son arme";
     return description;
   }
-
-  //this.armeActuelle;
 
   // lance la fct mvt() si index n'est ni un bloc ni un perso
   this.deplace = function(old_index, index, mvt, map) {
@@ -34,52 +32,43 @@ function Perso() {
       if ((tab_position[index] == "arme1Class") || (tab_position[index] == "arme2Class") || (tab_position[index] == "arme3Class") || (tab_position[index] == "arme4Class")) { // id des armes
         this.degat=map.arsenal[tab_position[index]].degat;
         alert("Vous vous équipez d'une arme avec "+this.degat+" de dégâts supplémentaires");
+        // déposé ici l'arme que l'on avait avant de s' équiper la nouvelle
         $carte.append("<div class='casevideClass' style='left:" + (index % 10) * 100 + "px; top:" + Math.floor(index / 10) * 100 + "px ;position: absolute;'></div>"); // on remplace la case de l'arme par une case vide
         
         if (mvt == "gauche") { // déplacement à gauche
           self.elm.css('left', parseInt(self.elm.css('left')) - 100);
         }
-
         else if (mvt == "droite") { // déplacement à droite
           self.elm.css('left', parseInt(self.elm.css('left')) + 100);
         }
-
         else if (mvt == "haut") { // déplacement en haut
           self.elm.css('top', parseInt(self.elm.css('top')) - 100);
         }
-
         else if (mvt == "bas") { // déplacement en bas
           self.elm.css('top', parseInt(self.elm.css('top')) + 100);
         }
         console.log(this.degat); // vérifie que les dégâts sont bien équivalent à la valeur de l'arme équipé
       }
 
-
       else if ((tab_position[index] == 3) || (tab_position[index] == 4)) { // "Alien" et 4 sont les id des personnages
         alert("FIGHT"); 
         fight(map.getPerso1(),map.getPerso2(),self); // ici fonction fight(), self = this.perso_actuel (ligne 25)
       }
 
-
       else { // si on se dirige vers une case vide
         if (mvt == "gauche") { // déplacement à gauche
           self.elm.css('left', parseInt(self.elm.css('left')) - 100);
         }
-
         else if (mvt == "droite") { // déplacement à droite
           self.elm.css('left', parseInt(self.elm.css('left')) + 100);
         }
-
         else if (mvt == "haut") { // déplacement en haut
           self.elm.css('top', parseInt(self.elm.css('top')) - 100);
         }
-
         else if (mvt == "bas") { // déplacement en bas
           self.elm.css('top', parseInt(self.elm.css('top')) + 100);
         }
-        //console.log(this.degat); // retourne la valeur des dégats.
       }
-
 
       if (self.nom == "Alien") tab_position[index] = 3; // maj index de la case suivante (contient maintenant le perso)      
       if (self.nom == "Predator") tab_position[index] = 4; // maj index de la case suivante (contient maintenant le perso)
