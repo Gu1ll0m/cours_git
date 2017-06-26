5d42a22bc92d93c1a5cef87ac29bb387555cc566
Gu1ll0m <gzucchi.pro@gmail.com>
2017-06-26 11:46:12 +0200

    Update fight_v2.js
    Ajout posture offensive et défensive

--- v2/js/fight_v2.js
+++ v2/js/fight_v2.js
@@ -27,20 +27,25 @@ function fight(perso1, perso2, perso_actuel) {
 
     if (choix == 1) {attaquer();}
     if (choix == 2) {defendre();}
-    else {console.log("Vous avez saisi un autre chiffre que 1 ou 2, le programme ne comprend pas votre choix !");};
 
+    //else {console.log("Vous avez saisi un autre chiffre que 1 ou 2, le programme ne comprend pas votre choix !");};
 
 
     // déroulement de l' attaque
     function attaquer() {
 
+      // posture de la cible
+      if(perso_actuel.posture == 1) { 
+      this.cible.degat = this.cible.degat*2;  
+            perso_actuel.posture = 0;
+    }
 
       if (this.cible.sante > 0) { // la cible à des points de vie
 
         var degats = this.game.perso_actuel.degat;
         alert(this.game.perso_actuel.nom + " attaque " + this.cible.nom + " et lui fait " + degats + " points de dégâts");
-        this.cible.sante == this.cible.sante - this.game.perso_actuel.degat; // les points de vie de la cible sont diminué de la valeur des dégâts de l'arme
-        if (this.cible.sante >= 0) {
+        this.cible.sante = this.cible.sante - this.game.perso_actuel.degat; // les points de vie de la cible sont diminué de la valeur des dégâts de l'arme
+        if (this.cible.sante > 0) {
           // gérer ici le changement de perso
           alert(this.cible.nom + " a encore " + this.cible.sante + " points de vie");
         }
@@ -50,20 +55,21 @@ function fight(perso1, perso2, perso_actuel) {
         }
       }
       if (this.cible.sante <= 0){ // permet de recommencer la partie
-        alert(this.game.perso_actuel.nom + " ne peut pas attaquer : il est mort! La partie est terminé.");
+        alert(this.cible.nom + " ne peut pas attaquer : il est mort! La partie est terminé.");
         alert("Pour rejouer appuyer sur F5");
       }
     };
 
-    // fonctionnement de la défense : division des dégats subies par 2
+    // déroulement de la défense
     function defendre() {
 
-      console.log(this.cible);
+    if(perso_actuel.posture == 0){ // si notre perso est en posture d' attaque
+    perso_actuel.posture = 1; // on la passe en posture de défense 
+    this.cible.degat=this.cible.degat/2; // les dégats de la cible sont divisé par 2
 
-      var def = (this.cible.degat / 2);
-      console.log(this.game.perso_actuel.nom + " divise les dégats de " + this.cible.nom +" par 2, ces dégâts actuels sont de " + def);
-      return def;
+      console.log(this.cible.degat);
     }
+  }
 
     break;
   };
